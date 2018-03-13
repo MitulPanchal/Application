a7c8c7aff3d4205365f867e95f46d21c22518f20
Sagar Pandav <pandavsagar123@gmail.com>
2018-03-13 23:08:39 +0530

    API Binding in Login Activity
    Added Api Call in LoginActivity

--- app/src/main/java/com/example/mitul/application/LoginActivity.java
+++ app/src/main/java/com/example/mitul/application/LoginActivity.java
@@ -1,25 +1,41 @@
 package com.example.mitul.application;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mitul.application.api.client.MyClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class LoginActivity extends AppCompatActivity {
 
     public DatabaseHelper databaseHelper;
+    private EditText email;
+    private ConstraintLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText email = findViewById(R.id.editTextEmail);
+        rootLayout = findViewById(R.id.rootLayoutLogin);
+
+        email = findViewById(R.id.editTextEmail);
         final EditText password = findViewById(R.id.editTextPassword);
         final Button buttonLogin = findViewById(R.id.buttonSignIn);
         final TextView textViewSignUp = findViewById(R.id.textViewSignUp);
@@ -59,5 +75,52 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
+
+        //TODO: Call this method from login button onclick
+        verifyUser();
+    }
+
+    public void verifyUser(){
+        String url = "https://localhost:56846/api/";    //Actual Url
+        String testUrl = "https://randomuser.me/api/";  //Test Url
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(testUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MyClient client = retrofit.create(MyClient.class);
+
+        //TODO: Uncomment below sentence(Actual Call) and Comment/Remove Test Call
+
+        //Call call = client.getPassword(email.getText().toString());  //Actual Call
+
+        Call call = client.getTestData(); //Test Call
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                Log.e("Response :", "yes");
+
+                String dataString = response.body().toString();
+
+                //TODO: Compare Password with dataString and return Result Success or failure below
+                Log.e("Data From Url", dataString);
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Snackbar.make(rootLayout, "Network Error", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Try Again", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                verifyUser();
+                            }
+                        })
+                        .setActionTextColor(Color.RED)
+                        .show();
+            }
+        });
     }
 }
+
